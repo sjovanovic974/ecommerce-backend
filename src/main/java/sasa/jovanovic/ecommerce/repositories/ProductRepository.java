31869a9fff59805eea7fb4059aa9934ef4070e96
sjovanovic974 <sjovanovic974@gmail.com>
2021-12-08 14:01:26 +0100

    Exposed ids in config file

--- src/main/java/sasa/jovanovic/ecommerce/repositories/ProductRepository.java
+++ src/main/java/sasa/jovanovic/ecommerce/repositories/ProductRepository.java
@@ -1,12 +1,15 @@
 package sasa.jovanovic.ecommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import sasa.jovanovic.ecommerce.models.Product;
 
 @Repository
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
